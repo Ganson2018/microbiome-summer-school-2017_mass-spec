938d65342d3f217c139368997e725ac379718da3
Francis Brochu <frank.brochu.2@gmail.com>
2017-06-19 09:09:02 -0400

    Modified install and added install script without conda

--- install_without_conda.sh
+++ install_without_conda.sh
@@ -2,7 +2,12 @@
 #author = Francis Brochu
 #Microbiome Summer School 2017
 
-conda create -n ms python=3.5 anaconda
+sudo apt-get install virtualenv && \
+sudo apt-get install python3-pip && \
+pip3 install numpy --upgrade && \
+pip3 install scikit-learn --upgrade && \
+pip3 install h5py --upgrade && \
+pip3 install jupyter --upgrade
 
 cd example/tutorial_code/cpp_extensions
 
