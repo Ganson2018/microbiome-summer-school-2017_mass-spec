8ffc843d11a0439e882e17ce2dbdcad10899ae25
Francis Brochu <frank.brochu.2@gmail.com>
2017-06-21 10:08:13 -0400

    Fixed install without conda

--- install_without_conda.sh
+++ install_without_conda.sh
@@ -3,10 +3,11 @@
 #Microbiome Summer School 2017
 
 sudo apt-get install python3-pip && \
-sudo apt-get install pyhton3-venv && \
+sudo apt-get install python3-venv && \
 python3 -m venv ./ms && \
 source ms/bin/activate && \
 pip3 install numpy --upgrade && \
+pip3 install scipy --upgrade && \
 pip3 install scikit-learn --upgrade && \
 pip3 install h5py --upgrade && \
 pip3 install jupyter --upgrade
